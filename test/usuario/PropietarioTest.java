2e1eefdd05f6d846409c2fa286389bb462913d1f
Bruno Chacana <chacanabruno@gmail.com>
2024-11-07 15:37:20 -0300

    agrego clase test Propietario

---
+++ test/usuario/PropietarioTest.java
@@ -0,0 +1,14 @@
+package usuario;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PropietarioTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
